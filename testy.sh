6c34b959664ff47e942cf4d0e5e6df2860bbcdc5
mazurkiewiczm <33000257+mazurkiewiczm@users.noreply.github.com>
2019-05-17 23:07:16 +0200

    Update testy.sh
    Dodanie psucia za pomoca szumu i zmiany rozdzielczosci

--- testy.sh
+++ testy.sh
@@ -2,7 +2,9 @@
 
 srcDir=$1
 exposure=$2
-blur=$3
+blur=$3  
+noise=$4 
+resize=$5 
 
 for filename in "$srcDir"/*.avi; do
 		basePath=${filename%.*}
@@ -11,13 +13,13 @@ for filename in "$srcDir"/*.avi; do
 		#emulacja jasnoscia i blurem
 		if [ $exposure == 0 ]
 		then
-			ffmpeg -i "$filename" -vf "eq=brightness=0, unsharp=lx=3:ly=3:la=$blur" out/out_"$baseName"_"$exposure"_"$blur".avi
+			ffmpeg -i "$filename" -vf "eq=brightness=0, unsharp=lx=3:ly=3:la=$blur, noise=alls=$noise:allf=t, scale=iw*$resize:ih*$resize" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize".avi
 		elif [ $exposure == '-' ]
 		then
-			ffmpeg -i "$filename" -vf "eq=brightness=-0.5, unsharp=lx=3:ly=3:la=$blur" out/out_"$baseName"_"$exposure"_"$blur".avi
+			ffmpeg -i "$filename" -vf "eq=brightness=-0.5, unsharp=lx=3:ly=3:la=$blur, noise=alls=$noise:allf=t, scale=iw*$resize:ih*$resize" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize".avi
 		elif [ $exposure == '+' ]
 		then
-			ffmpeg -i "$filename" -vf "eq=brightness=0.5, unsharp=lx=3:ly=3:la=$blur" out/out_"$baseName"_"$exposure"_"$blur".avi
+			ffmpeg -i "$filename" -vf "eq=brightness=0.5, unsharp=lx=3:ly=3:la=$blur, noise=alls=$noise:allf=t, scale=iw*$resize:ih*$resize" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize".avi
 		fi
 		
 done
