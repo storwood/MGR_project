eff24886e07ae1bb0c5f0e4b772e4ed528119f43
Kamil <kamilkawa1994@gmail.com>
2019-05-21 20:36:00 +0200

    Motion blur added, check README for details

--- README.md
+++ README.md
@@ -8,3 +8,40 @@ blur   -
 noise  - <0,25,50,75,100>; 0 brak szumu, 100 bardzo zaszumione video
 resize - <0...1>; 0 to 0px x 0px, 1 to orginalny rozmiar, 0.5 rozdzielczosc/2
 #dodalem codec'a coby lepiej konwertowa
+
+Available values for motion blur are:
+
+‘addition’
+‘grainmerge’
+‘and’
+‘average’
+‘burn’
+‘darken’
+‘difference’
+‘grainextract’
+‘divide’
+‘dodge’
+‘freeze’
+‘exclusion’
+‘extremity’
+‘glow’
+‘hardlight’
+‘hardmix’
+‘heat’
+‘lighten’
+‘linearlight’
+‘multiply’
+‘multiply128’
+‘negation’
+‘normal’
+‘or’
+‘overlay’
+‘phoenix’
+‘pinlight’
+‘reflect’
+‘screen’
+‘softlight’
+‘subtract’
+‘vividlight’
+‘xor’
+
--- testy.sh
+++ testy.sh
@@ -4,7 +4,8 @@ srcDir=$1
 exposure=$2
 blur=$3  
 noise=$4 
-resize=$5 
+resize=$5
+motion_blur=$6 
 
 for filename in "$srcDir"/*.avi; do
 		basePath=${filename%.*}
@@ -13,13 +14,25 @@ for filename in "$srcDir"/*.avi; do
 		#emulacja jasnoscia i blurem
 		if [ $exposure == 0 ]
 		then
-			ffmpeg -i "$filename" -vf "eq=brightness=0, unsharp=lx=3:ly=3:la=$blur, noise=alls=$noise:allf=t, scale=iw*$resize:ih*$resize" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize".avi
+			ffmpeg -i "$filename" -vf "eq=brightness=0, 
+						   unsharp=lx=3:ly=3:la=$blur,
+						   noise=alls=$noise:allf=t,
+						   scale=iw*$resize:ih*$resize,
+						   tblend=all_mode=$motion_blur" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize"_"$motion_blur".avi
 		elif [ $exposure == '-' ]
 		then
-			ffmpeg -i "$filename" -vf "eq=brightness=-0.5, unsharp=lx=3:ly=3:la=$blur, noise=alls=$noise:allf=t, scale=iw*$resize:ih*$resize" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize".avi
+			ffmpeg -i "$filename" -vf "eq=brightness=-0.5,
+						   unsharp=lx=3:ly=3:la=$blur,
+						   noise=alls=$noise:allf=t,
+						   scale=iw*$resize:ih*$resize, 
+						   tblend=all_mode=$motion_blur" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize"_"$motion_blur".avi
 		elif [ $exposure == '+' ]
 		then
-			ffmpeg -i "$filename" -vf "eq=brightness=0.5, unsharp=lx=3:ly=3:la=$blur, noise=alls=$noise:allf=t, scale=iw*$resize:ih*$resize" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize".avi
+			ffmpeg -i "$filename" -vf "eq=brightness=0.5,
+						   unsharp=lx=3:ly=3:la=$blur,
+						   noise=alls=$noise:allf=t,
+						   scale=iw*$resize:ih*$resize,
+						   tblend=all_mode=$motion_blur" -vcodec libx264 out/out_"$baseName"_"$exposure"_"$blur"_"$noise"_"$resize"_"$motion_blur".avi
 		fi
 		
 done
